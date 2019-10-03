0e616fd3a378c9afe3128b4242ce0ae34115b372
Gosia <kowal.malgorzata87@gmail.com>
2019-10-03 09:17:30 +0200

    another conflicts fix

--- src/service/basket.test.js
+++ src/service/basket.test.js
@@ -17,14 +17,13 @@ test('add product to basket', () => {
     expect(basket.get().products.length).toEqual(1);
     
     expect(basket.get().total).toEqual(1255);
-<<<<<<< HEAD
+
     
 });test('that multiple products can be added to basket', () => {
-=======
+
 });
 
 test('that multiple products can be added to basket', () => {
->>>>>>> a3a324ceeefa5b686a59d48d6b7744262a7dbe50
   // todo: basket will already have a product inside from the previous test, we want to test that another product can be added
 
   // todo: assert that products amount increased
@@ -40,10 +39,3 @@ test('that product can be removed from the basket', () => {
 
 //todo: write test to check basket.applyDiscount(20) works ?
 //todo: write test to check basket.clear() works ?
-<<<<<<< HEAD
-
-
-  
-
-=======
->>>>>>> a3a324ceeefa5b686a59d48d6b7744262a7dbe50
