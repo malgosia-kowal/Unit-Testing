90701b700daaaf7d66f7be8b74bca1749e7b4a31
Lukas <kowal.lukas88@gmail.com>
2019-10-04 15:02:02 +0100

    base angular set up

---
+++ ecommerce/src/app/products/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  name: string;
+  price: number;
+  quantity: number;
+  size: string;
+  image: string;
+}
---
+++ ecommerce/src/app/products/products.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "./product";
+
+const products:Product[] = [
+  { name:'nikey', price: 123, quantity: 1, size: '8', image: 'http://placekitten.com/200/300'},
+  { name:'nikey', price: 123, quantity: 1, size: '8.5', image: 'http://placekitten.com/200/300'},
+  { name:'rebook', price: 123, quantity: 1, size: '9', image: 'http://placekitten.com/200/300'},
+  { name:'cream', price: 123, quantity: 1, size: '10', image: 'http://placekitten.com/200/300'},
+];
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  product: Product = {
+    name:'nikey',
+    price: 123,
+    quantity: 1,
+    size: '8',
+    image: 'http://placekitten.com/200/300',
+  };
+  constructor() {
+    console.log(this.product);
+  }
+
+  ngOnInit() {
+  }
+
+}
