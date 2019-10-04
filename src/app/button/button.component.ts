326b47e21f6aa3a52f34165779542c18476c4ba7
Lukas <kowal.lukas88@gmail.com>
2019-10-04 18:05:38 +0100

    replaced add to basket button with component

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -8,7 +8,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() text: string;
   @Input() styles: any;
-  @Output() action: EventEmitter<string> = new EventEmitter<string>();
+  @Output() action: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
   constructor() { }
 
   ngOnInit() {
