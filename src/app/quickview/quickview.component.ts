18a79fc21efba62e2877abc6fa4b1ac1c2940095
Lukas <kowal.lukas88@gmail.com>
2019-10-08 07:17:58 +0100

    added products to quickview

--- src/app/quickview/quickview.component.ts
+++ src/app/quickview/quickview.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {Toggable, ToggleService} from '../service/toggle.service';
+import { Component, OnInit } from '@angular/core';
+import { Toggable, ToggleService } from '../service/toggle.service';
+import { Product } from '../products/product';
+import { BasketService } from '../service/basket.service';
 
 @Component({
   selector: 'app-quickview',
@@ -8,12 +10,14 @@ import {Toggable, ToggleService} from '../service/toggle.service';
 })
 export class QuickviewComponent implements OnInit {
   visible: boolean;
-  constructor(public toggleService: ToggleService) { }
+  products: Product[];
+  constructor(public toggleService: ToggleService, private basketService: BasketService) { }
 
   ngOnInit() {
     this.toggleService.visible(Toggable.Quickview).subscribe(visible => {
       this.visible = visible;
     });
+    this.products = this.basketService.products;
   }
 
 }
