2cea39b63c12e1693a957cf831cdd871856003a4
Gosia <kowal.malgorzata87@gmail.com>
2019-10-19 21:57:29 +0200

    e2e tests after changes front

--- cypress/integration/products_page.spec.js
+++ cypress/integration/products_page.spec.js
@@ -5,8 +5,78 @@ describe('My First Test', () => {
   // 3. Interact with that element.
   // 4. Assert about the content on the page.
  
-  it('Visits the products page', () => {
-    cy.visit('http://localhost:4200/');
+  describe('My First Test', () => {
+
+    it('Visits the products page', () => {
+      cy.visit('http://localhost:4200/');
+  
+      cy.get('app-products').should('exist');
+      cy.get('app-quickview').should('exist');
+      cy.get('app-basket').should('exist');
+    });
+  
+    it('Add product to the basket', () => {
+      cy.get('[id="addToBasketButton-0"]')
+          .click();
+      // TODO: this assertion relies on mockend
+      // we should find a way to make it more robust
+      cy.get('[id="basketTotal"]').should('contain', '123')
+    }); 
+  
+    it('Add the same product to the basket', () => {
+      cy.get('button').contains('Add to basket')
+          .click();
+      cy.get('button').contains('Add to basket')
+          .click();
+  
+      cy.get('[id="basketLength"]').should('contain', '1');
+      cy.get('[id="basketTotal"]').should('contain', '246');
+    }); 
+  
+    it('Remove product from basket', () => {
+      cy.get('button').contains('Add to basket')
+          .click();
+      cy.get('button').contains('Clean')
+          .click();
+          
+      cy.get('[id="basketLength"]').should('contain', '0');
+      
+      cy.get('[id="basketTotal"]').should('contain', '0');
+    }); 
+  
+    it('add two different products to basket', () => {
+      cy.get('[id="addToBasketButton-0"]')
+          .click();
+      cy.get('[id="addToBasketButton-1"]')
+          .click();
+  
+      cy.get('[id="basketLength"]').should('contain', '2');
+      cy.get('[id="basketTotal"]').should('contain', '246');
+    }); 
+  
+    it('open quickView and check if it is visible', () => {
+      cy.get('[id="quickViewButton"]')
+          .click();
+  
+      cy.get('.quickviewContainer').should('be.visible')  
+    });
+      
+    it('close quickView and check if it is not visible', () => {
+      cy.get('[id="quickViewButton"]')
+          .click();
+  
+      cy.get('.quickviewContainer').should('not.be.visible');  
+      
+    }); 
+  
+    it('clean basket', () => {
+    
+      cy.get('[id="clearTheBasketButton"]')
+          .click();
+  
+      cy.get('[id="basketTotal"]').should('contain', '0');
+    });
+  
   });
 
 });
